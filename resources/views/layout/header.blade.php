17333f074b82a39477c8a3cf0b747ca6d8d83aad
ShefaliwedsHarish <hariom1gyan@gmail.com>
2024-09-26 21:38:07 +0530

    Header update

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -21,7 +21,7 @@
     
         <nav class="navbar navbar-expand-lg  fixed-top change_header hs_navbar_part">
             <div class="container-fluid">
-              <a class="navbar-brand  " href="#">Navbar</a>
+              <a class="navbar-brand  " href="#">HSGroup</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
@@ -38,10 +38,10 @@
                       Online Service
                     </a>
                     <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><a class="dropdown-item" href="#">Online Service</a></li>
+                      <li><a class="dropdown-item" href="#">Itr </a></li>
                       <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                      <li><a class="dropdown-item" href="#">Printing</a></li>
                     </ul>
                   </li>
                   <li class="nav-item">
