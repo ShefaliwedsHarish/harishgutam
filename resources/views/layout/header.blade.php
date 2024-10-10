7168bac98a68718fc15b643234c9cf4d2d497a0e
ShefaliwedsHarish <hariom1gyan@gmail.com>
2024-10-10 06:09:21 +0530

    register template

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -52,7 +52,7 @@
                   </li>
 
                   <li class="nav-item">
-                    <a class="nav-link" href="{{route('contact_us')}}">Register</a>
+                    <a class="nav-link" data-bs-target="#register_from" data-bs-toggle="modal" href="#">Register</a>
                   </li>
         
 
@@ -107,6 +107,8 @@
                         <!-- Submit button -->
                         <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg btn-block">Sign in</button>
               
+                        <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg btn-block" data-bs-target="#register_from" data-bs-toggle="modal">Register</button>
+              
                         <div class="divider d-flex align-items-center my-4">
                           <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
                         </div>
@@ -129,31 +131,138 @@
       </div>
         
 
-      {{-- forgot password --}}
 
-    
-      
+
+      {{-- forgot password --}}
       <!-- Modal -->
-      <div class="modal fade" id="forgot_password" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div class="modal top fade" id="forgot_password" tabindex="-1" aria-labelledby="exampleModalLabel"
+      aria-hidden="true" data-mdb-backdrop="true" data-mdb-keyboard="true">
+     <div class="modal-dialog" style="width: 300px;">
+         <div class="modal-content text-center">
+             <div class="modal-header h5 text-white bg-primary justify-content-center">
+                 Password Reset
+             </div>
+             <div class="modal-body px-5">
+                 <p class="py-2">
+                     Enter your email address and we'll send you an email with instructions to reset your password.
+                 </p>
+                 <div data-mdb-input-init class="form-outline">
+                     <input type="email" id="typeEmail" class="form-control my-3" />
+                     <label class="form-label" for="typeEmail">Email input</label>
+                 </div>
+                 <a href="#" data-mdb-ripple-init class="btn btn-primary w-100">Reset password</a>
+                 <div class="d-flex justify-content-between mt-4">
+                  <a class="nav-link" data-bs-target="#login_user" data-bs-toggle="modal" href="#">Login</a>
+                     <a class=""data-bs-target="#register_from" data-bs-toggle="modal"  href="#">Register</a>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+
+    {{-- endforgot --}}
+      
+
+    {{-- Regester form  --}}
+
+    <div class="modal fade" id="register_from" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Registration</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+        <section class="h-100 bg-dark">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col">
+        <div class="card card-registration my-4">
+          <div class="row g-0">
+            <div class="col-xl-6 d-none d-xl-block">
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img4.webp"
+                alt="Sample photo" class="img-fluid"
+                style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;" />
             </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+            <div class="col-xl-6">
+              <div class="card-body p-md-5 text-black">
+                <h3 class="mb-5 text-uppercase">Welcome HS Group </h3>
+
+                <div class="row">
+                  <div class="col-md-6 mb-4">
+                    <div data-mdb-input-init class="form-outline">
+                      <input type="text" id="form3Example1m" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example1m">First name</label>
+                    </div>
+                  </div>
+                  <div class="col-md-6 mb-4">
+                    <div data-mdb-input-init class="form-outline">
+                      <input type="text" id="form3Example1n" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example1n">Last name</label>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-6 mb-4">
+                    <div data-mdb-input-init class="form-outline">
+                      <input type="number" id="form3Example1m1" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example1m1">Phone Number </label>
+                    </div>
+                  </div>
+                  <div class="col-md-6 mb-4">
+                    <div data-mdb-input-init class="form-outline">
+                      <input type="email" id="form3Example1n1" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example1n1">Email</label>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-md-6 mb-4">
+                    <div data-mdb-input-init class="form-outline">
+                      <input type="password" id="form3Example1m1" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example1m1">Password  </label>
+                    </div>
+                  </div>
+                  <div class="col-md-6 mb-4">
+                    <div data-mdb-input-init class="form-outline">
+                      <input type="password" id="form3Example1n1" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Example1n1">Conform Password</label>
+                    </div>
+                  </div>
+                </div>
+
+                <div data-mdb-input-init class="form-outline mb-4">
+                  <input type="file" id="form3Example8" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example8">Profile</label>
+                </div>
+
+
+
+                <div class="d-flex justify-content-end pt-3">
+                  <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-light btn-lg" data-bs-target="#login_user" data-bs-toggle="modal">Login</button>
+                  <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-warning btn-lg ms-2">Submit form</button>
+                </div>
+
+              </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
+  </div>
+</section>
+          </div>
+          <div class="modal-footer">
+            {{-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button> --}}
+          </div>
+        </div>
+      </div>
+    </div>
 
-    {{-- endforgot --}}
-      
+    {{-- end register form --}}
       
       
       
