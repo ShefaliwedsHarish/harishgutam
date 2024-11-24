d4f0b6457e7b7c2f74dbf2dd128bd3d041d1d48a
ShefaliwedsHarish <hariom1gyan@gmail.com>
2024-11-24 17:03:07 +0530

    testing

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -9,6 +9,7 @@ $route=config('path.admin')
    
 @section('content')
      <!-- Sale & Revenue Start -->
+     {{-- testing for this  --}}
       <div class="container-fluid pt-4 px-4">
         <div class="row g-4">
             <div class="col-sm-6 col-xl-3">
